6ce6a3fda7d3ea608a50ef3898e5d390cbd381c5
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-11-11 01:14:38 +0530

    Completed with Birthday reminder and Tour Display projects

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,7 +2,37 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const newObj = {}
+  return <main>
+    <section className="container">
+      <div class="flexcontainer">
+        <h3>{people.length} birthdays today</h3>
+        <button class="addbutton"
+          onClick={() => {
+            // data.push({
+            //   id: 6,
+            //   name: 'Bertie Yates',
+            //   age: 29,
+            //   image:
+            //     'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+            // });
+
+            // setPeople(data);
+            
+            // console.log(data);
+            // console.log(people);
+            
+          }}
+        >+</button>
+      </div>
+      <List people={people}>
+      </List>
+      <button onClick={() => setPeople([])}>
+        Clear all
+    </button>
+    </section>
+  </main>;
 }
 
 export default App;
--- 2-tours/setup/src/Loading.js
+++ 2-tours/setup/src/Loading.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className="loading">
-      <h1>loading...</h1>
+      {/* <h1>loading...</h1> */}
+      <div class="flexcontainer">
+        <h1 class="loader">Loading</h1>
+        {/* <div class="animatedloader"></div> */}
+        <div class="animatedloader2"></div>
+        <div class="animatedloader3"></div>
+        <div class="animatedloader4"></div>
+        <div class="animatedloader5"></div>
+      </div>
     </div>
   );
 };
