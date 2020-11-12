f95d860fb45f26c18dbd33efcba6638d184f5d8d
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-11-12 23:18:10 +0530

    Added Hotel Menu Project

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+
+  return (
+    // <div className="btn-container">
+    //   <button className="filter-btn"
+    //     onClick={() => {
+    //       filterItems('all')
+    //     }}
+    //   >
+    //     All
+    //   </button>
+
+    //   <button className="filter-btn"
+    //     onClick={() => {
+    //       filterItems('breakfast')
+    //     }}
+    //   >
+    //     {/* {filterItems} */}
+    //     Breakfast
+    //   </button>
+    // </div>
+
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return <button type="button" className="filter-btn"
+          key={index}
+          onClick={() => {
+            filterItems(category)
+          }}
+        >
+          {category}
+        </button>
+
+      })}
+    </div>
+  );
 };
 
 export default Categories;
